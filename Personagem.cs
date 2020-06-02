23734b21f1d58b6679edc11213c3f69d45be909a
unknown <renanescudeiro1@gmail.com>
2020-06-02 19:00:07 -0300

    Tony Stark

---
+++ Personagem.cs
@@ -0,0 +1,18 @@
+namespace Aula3
+{
+    public class Personagem
+    {
+        public string nome = "Tony Stark";
+
+        public int idade = 46;
+
+        public string armadura;
+        
+        public string Atacar()
+        {
+            return = "O personagem atacou";
+        }
+    }
+
+
+}
